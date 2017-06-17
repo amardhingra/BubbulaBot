ee9eabfc76f00a19aa76b11ead0f8322a86adba3
Amar Dhingra <amardhingra@gmail.com>
2017-06-17 15:55:55 -0400

    Created install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BubbulaBot',
+      version='0.1',
+      description='A chatbot for the special Bubbula in your life.',
+      author='Amar Dhingra, David Watkins',
+      author_email='amardhingra@gmail.com, davidwatkins@cs.columbia.edu',
+      url='https://github.com/amardhingra/BubbulaBot',
+      install_requires=['chatterbot']
+      )
